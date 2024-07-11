fb9e33300ec0988aefc4699a3d324d8eae487a3f
Alejandro-biohpc <alrodriguezmartinez@gmail.com>
2024-07-11 10:34:43 +0200

    Some problems fixed

--- ASGARD/analyze_trajectory/get_results.py
+++ ASGARD/analyze_trajectory/get_results.py
@@ -6,7 +6,7 @@ import os
 os.environ['GMX_MAXBACKUP'] = "-1"
 os.environ['GMXLIB'] = os.getcwd()+"/ASGARD/external_sw/gromacs/force_field/" #required for g_mmpbsa
 
-if len(sys.argv) != 4:
+if len(sys.argv) < 4:
     print("\nDebe introducir:")
     print("Absolute path of the folder with the calculations + the suffix")
     print("Profile: [ TARGET_QUERY | TARGET | TARGET_QUERIES | QUERIES | DNA_QUERY ]  ")
@@ -16,15 +16,25 @@ if len(sys.argv) != 4:
     exit()
 try:
     out_molec = sys.argv[1]
-    profile = sys.argv[2].upper()
-    gromacs = sys.argv[3]
+    profile =   sys.argv[2].upper()
+    gromacs =   sys.argv[3]
+    ligand = None
+    reference = None
+
+    for arg in sys.argv[4:]:
+        if arg.startswith("ligand="):
+            ligand = arg.split("=")[1]
+        elif arg.startswith("reference="):
+            reference = arg.split("=")[1]
 except ValueError:
     print ("Error con la entra de datos")
     exit()
+
+
 #
 # Create configuration object
 #
-cfg = ConfigHolder(out_molec, profile, gromacs)
+cfg = ConfigHolder(out_molec, profile, gromacs, ligand, reference)
 Resume(cfg)
 
 #
